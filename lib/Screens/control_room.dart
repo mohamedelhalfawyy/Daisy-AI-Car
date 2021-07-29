724407850a08fff290ee67bc30dae6eb6179b70e
sebastiano2000 <sebastianosaad@gmail.com>
2021-07-29 02:38:23 +0200

    connection and voice

--- lib/Screens/control_room.dart
+++ lib/Screens/control_room.dart
@@ -1,7 +1,11 @@
 import 'dart:io';
 import 'package:animated_text_kit/animated_text_kit.dart';
+import 'package:convex_bottom_bar/convex_bottom_bar.dart';
 import 'package:flutter/material.dart';
+import 'package:graduation_project/Screens/voiceControl.dart';
 import 'package:graduation_project/widgets/app_button.dart';
+import 'package:flutter_bluetooth_serial/flutter_bluetooth_serial.dart';
+import 'package:graduation_project/Services//connection.dart';
 import 'DashBoard.dart';
 import 'dart:math' as math;
 
@@ -14,6 +18,11 @@ class ControlRoom extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final double mirror = math.pi;
+    return MaterialApp(
+        home: FutureBuilder(
+        future: FlutterBluetoothSerial.instance.requestEnable(),
+    builder: (context, future) {
+    if (future.connectionState == ConnectionState.waiting) {
     return Scaffold(
       backgroundColor: Color(0XFFC7FFBE),
       body: SafeArea(
@@ -43,7 +52,7 @@ class ControlRoom extends StatelessWidget {
                         fontStyle: FontStyle.italic,
                         fontSize: 30,
                       ),
-                      speed: Duration(milliseconds: 500))
+                          speed: Duration(milliseconds: 500))
                     ],
                     totalRepeatCount: 1,
                     displayFullTextOnTap: true,
@@ -69,6 +78,49 @@ class ControlRoom extends StatelessWidget {
           ),
         ),
       ),
+      bottomNavigationBar: ConvexAppBar(
+        style: TabStyle.react,
+        items: [
+          TabItem(icon: Icons.home, title: 'Home'),
+          TabItem(icon: Icons.videogame_asset, title: 'Control'),
+          TabItem(icon: Icons.assignment_late_outlined, title: 'About Us'),
+          TabItem(icon: Icons.logout, title: 'Log Out'),
+        ],
+        initialActiveIndex: 1,
+        backgroundColor: Colors.blueGrey,
+        onTap: (int i) => print('click index=$i'),
+      ),
     );
+    } else {
+    return Home();
+    }
+    },
+    ),
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+        child: Scaffold(
+          appBar: AppBar(
+            title: Text('Connection'),
+          ),
+          body: SelectBondedDevicePage(
+            onCahtPage: (device1) {
+              BluetoothDevice device = device1;
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) {
+                    return ChatPage(server: device);
+                  },
+                ),
+              );
+            },
+          ),
+        ));
   }
 }
